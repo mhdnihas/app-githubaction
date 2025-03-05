fab593e7ffb31d38ff9b5707ddac390d69736ea3
mhdnihas <muhammednihas8589@gmail.com>
2025-03-05 08:37:29 +0530

    small changes

---
+++ tests/test_operations.py
@@ -0,0 +1,14 @@
+from src.Math_operations import add,sub
+
+def test_add():
+    assert add(1,2)==3
+    assert add(0,0)==0
+    assert add(-1,-1)==-2
+
+def test_sub():
+    assert sub(1,2)==-1
+    assert sub(0,0)==0
+    assert sub(-1,-1)==0
+    assert sub(1,-1)==2
+
+    
